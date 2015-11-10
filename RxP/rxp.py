bf9f470be4ab05c9dda457f71d6b8927c98478e5
Kenny Shu <kshu6@gatech.edu>
2015-11-10 18:04:53 -0500

    function signatures

--- RxP/rxp.py
+++ RxP/rxp.py
@@ -4,5 +4,44 @@ Created on Nov 10, 2015
 @author: Kenny Shu, Wesley Wong
 '''
 
-def socket():
-    pass
+import socket
+
+class rxp(socket.socket):
+    _recv_buffer = []
+    _send_buffer = []
+    
+    _header = b''
+    
+    
+    def socket(self):
+        pass
+    
+    def bind(self, address):
+        pass
+    
+    def connect(self, address):
+        pass
+    
+    def listen(self):
+        pass
+    
+    def accept(self):
+        pass
+    
+    def send(self, byte_string):
+        pass
+
+    def receive(self, size):
+        pass
+    
+    def close(self):
+        pass
+    
+    def settimeime(self, seconds):
+        pass
+    
+    def gettimeout(self):
+        pass
+
+
+    
