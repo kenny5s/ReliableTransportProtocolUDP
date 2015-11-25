f15b2c9e14711286213a7c003f9403588a098740
wesleyeewong <wesleywong1993@gmail.com>
2015-11-25 16:07:09 -0500

    _generate_checksum function

--- RxP/udp/rxp.py
+++ RxP/udp/rxp.py
@@ -276,6 +276,19 @@ class RxpSocket:
         #TODO
         pass
 
+    # generates checksum
+    # takes in bytes
+    def _generate_checksum(packet):
+        s = 0
+        for i in range(0, len(msg), 2):
+            w = ord(msg[i]) + (ord(msg[i+1]) << 8)
+            s = carry_around_add(s,w)
+        return ~s & 0xffff
+
+    def carry_around_add(a, b):
+        c = a + b
+        return (c & 0xffff) + (c >> 16)
+
     # Puts data into the receive buffer and return the size of data
     def send(self, data):
         self._send_buffer.extend(data)
